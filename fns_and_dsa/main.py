d6ea6bb387976c7cec12a6f7ff567d90449164ea
Ruth Shimeles <ruthshimelesgetachew73@gmail.com>
2025-06-06 13:01:14 +0300

    perform_operations via main.py

---
+++ fns_and_dsa/main.py
@@ -0,0 +1,6 @@
+# test.py (just for you, don't submit this)
+from arithmetic_operations import perform_operation
+
+print(perform_operation(5, 6, 'add'))          # Should print 11.0
+print(perform_operation(10, 0, 'divide'))      # Should print "Error: Division by zero"
+print(perform_operation(3, 4, 'power'))        # Should print "Error: Invalid operation"
