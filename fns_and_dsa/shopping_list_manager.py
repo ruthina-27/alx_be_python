8cfa6d0884efcb2893eed8e460e85255b281e371
Ruth <157282840+ruthina-27@users.noreply.github.com>
2025-06-08 18:41:49 +0300

    fix shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -17,7 +17,7 @@ def main():
             continue
         
         if choice == 1:
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             
         elif choice == 2:
