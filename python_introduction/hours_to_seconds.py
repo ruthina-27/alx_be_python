554fa2b7c60907a84bbd5ef5d695f70eb69ecbdc
Ruth Shimeles Getachew <ruthshimelesgetachew73@gmail.com>
2025-06-19 11:58:32 +0300

    Remove all files (clean slate)

--- python_introduction/hours_to_seconds.py
+++
@@ -1,8 +0,0 @@
-# Define the number of hours
-hours = 2
-
-# Calculate the number of seconds in given hours
-seconds = hours * 3600
-
-# Print the result
-print(f"{hours} hour(s) is {seconds} seconds.")
--- python_introduction/rectangle_area.py
+++
@@ -1,9 +0,0 @@
-# Define the length and width
-length = 10
-width = 5
-
-# Calculate the area
-area = length * width
-
-# Print the result
-print(f"The area of the rectangle is: {area}")
